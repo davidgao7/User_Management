3d2144420d7f020a9b1c2bb5853601ed3612d809
David Gao <gao.ten@northeastern.edu>
2024-06-24 10:31:06 -0400

    create uuid for each user entry

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid'; // generate unique id
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        LastName: "Doe",
-        age: 25
-    },
-    {
-        firtName: "Jane",
-        lastName: "Doe",
-        age: 24
-    }
-]
+const users = []
 
 // all routes in usersRoutes will start with /users already
 router.get('/', (req, res) => {
@@ -26,9 +16,12 @@ router.post('/', (req, res) => {
     // console.log('POST ROUTE REACHED')
 
     // console.log(req.body) // get the body from postman (req.body
+
     // add users to the array
     const user = req.body;
-    users.push(user)
+
+    // add user id to the user object, use uuidv4 to generate unique id
+    users.push({ ...user, id: uuidv4() });
 
     // send response to postman
     res.send(`User with the name ${user.firstName} added to the database!`)
